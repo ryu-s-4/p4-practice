7be48402e77843acc68ca57c897f9115472396f2
ryuma <bo.works.0413@gmail.com>
2020-09-12 20:53:40 +0900

    2020-0912-2053

---
+++ control-plane/cp.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// P4runtime Client 定義
+
+	// P4runtime Client インスタンス生成
+
+	// Master 権限取得
+
+	// CLI で命令待ち受け（とりあえず指定した VLAN-ID の Counter 値取得の命令のみ）
+	// ガチで実装する場合は送受信の message キュー ＋ goroutine を用いた非同期での送受信待ち受け
+}
